f99283210407ab1afbfe5ac5f4260908bea25858
Dheeraj <ID+Dheeraj3535@users.noreply.github.com>
2025-03-10 19:46:41 +0530

    loop

---
+++ loop.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(;;){
+        cout << '\n';
+    int n;
+    cout<<"write a number";
+    cin>>n;
+
+    for (int abc = 1; abc <= n; abc = abc + 1) {
+        for (int space_count = n - abc; space_count >= 1; space_count--) {
+            cout << ' ';
+        }
+
+        for (int star_count = 1; star_count<abc; star_count++){ 
+            cout <<'*';
+        } 
+
+        for (int star_count =1; star_count<=abc; star_count++){
+            cout<<'*';
+        }
+        for (int space_count = n - abc; space_count >= 1; space_count--) {
+            cout << ' ';
+        }
+        cout<< '\n';
+    }
+    }
+
+    }
+    
