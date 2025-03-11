bb1a08378fdf5d36f99ada55b3d885a02a58dd3e
Dheeraj <ID+Dheeraj3535@users.noreply.github.com>
2025-03-11 09:48:22 +0530

    pattern 2 complete

--- loop.cpp
+++ loop.cpp
@@ -3,18 +3,16 @@ using namespace std;
 
 int main() {
     for(;;){
-       std:: cout << '\n';
-
     int pattern;
-    std::cout<<"enter the patten type";
+    std::cout<<"enter the patten type\n";
     cin>>pattern;
         int n;
-        std::cout<<" number of lines ";
+        std::cout<<" number of lines\n ";
     cin>>n;
     
     // this program is for triangular star pattern
 
-   if (pattern=1) {
+   if (pattern==1) {
     std::cout<<"you interested in first pattern \n";
 
     for (int abc = 1; abc <= n; abc = abc + 1) {
@@ -29,26 +27,27 @@ int main() {
     }
    }
 
-    else if(pattern=2){ 
-        std::cout<<"you are interested in second pattern";
+    else if(pattern==2){ 
+        std::cout<<"you are interested in second pattern\n";
     for (int abc = 1; abc <= n; abc = abc + 1) {
             for (int space_count = n - abc; space_count >= 1; space_count--) {
                 std::cout << ' ';
             }
     
-            for (int star_count = 1; star_count<abc; star_count++){ 
-                std::cout <<'*';
+            for (int star_count =1; star_count<abc; star_count++){ 
+                std::cout<<'*';
             } 
     for (int star_count =1; star_count<=abc; star_count++){
             std::cout<<'*';
         }
         for (int space_count = n - abc; space_count >= 1; space_count--) {
             std::cout << ' ';
- }          
+ }    
+         std::cout<<'\n';      
  }
 }
-    else {
-        std::cout<<"invalid";
+else {
+        std::cout<<"invalid\n";
     }
     }
 
